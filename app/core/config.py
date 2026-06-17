aa81259901d6e994363b8a30b1c569164e9a8788
Gabriel Anthony <gabrielanthony540@gmail.com>
2026-06-17 12:23:50 -0300

    config api openrounter

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # API Cada atributo é uma variável que será lida do .env
-    OPENROUTER_API_KEY: str | None = None # deixando opcional
+    openrouter_api_key: str | None = None # deixando opcional
     model_name: str = "nousresearch/hermes-3-llama-3.1-405b:free"
     max_tokens: int = 4096
     temperature: float = 0.7
